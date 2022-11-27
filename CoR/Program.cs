946d1b93a958f7d66b4a02efd1231014c87c3f6a
vasif <vasif@NIYAMEDDIN>
2022-11-27 17:59:58 +0400

    Add project files.

---
+++ CoR/Program.cs
@@ -0,0 +1,76 @@
+﻿public interface ITranslator
+{
+    void SetNext(ITranslator next);
+    bool IsTranslated(string languageFrom, string languageTo);
+}
+
+public class TranslatorJpToEn : ITranslator
+{
+    private ITranslator? _next;
+    private string _languageFrom = "japanese";
+    private string _languageTo = "english";
+
+    public void SetNext(ITranslator next) => _next = next;
+
+    public bool IsTranslated(string languageFrom, string languageTo)
+    {
+        if (languageFrom.ToLower() == _languageFrom && languageTo.ToLower() == _languageTo)
+            return true;
+
+        return _next is null ? false : _next.IsTranslated(languageFrom, languageTo);
+    }
+}
+
+public class TranslatorChToEn : ITranslator
+{
+    private ITranslator? _next;
+    private string _languageFrom = "chinese";
+    private string _languageTo = "english";
+
+    public void SetNext(ITranslator next) => _next = next;
+
+    public bool IsTranslated(string languageFrom, string languageTo)
+    {
+        if (languageFrom.ToLower() == _languageFrom && languageTo.ToLower() == _languageTo)
+            return true;
+
+        return _next is null ? false : _next.IsTranslated(languageFrom, languageTo);
+    }
+}
+
+public class TranslatorGerToEn : ITranslator
+{
+    private ITranslator? _next;
+    private string _languageFrom = "germany";
+    private string _languageTo = "english";
+
+    public void SetNext(ITranslator next) => _next = next;
+
+    public bool IsTranslated(string languageFrom, string languageTo)
+    {
+        if (languageFrom.ToLower() == _languageFrom && languageTo.ToLower() == _languageTo)
+            return true;
+
+        return _next is null ? false : _next.IsTranslated(languageFrom, languageTo);
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string from = "Chinese";
+        string to = "German";
+
+
+        ITranslator translator1 = new TranslatorGerToEn();
+        ITranslator translator2 = new TranslatorChToEn();
+        ITranslator translator3 = new TranslatorJpToEn();
+
+        translator1.SetNext(translator2);
+        translator2.SetNext(translator3);
+
+        var result = translator1.IsTranslated(from, to);
+        Console.WriteLine(result);
+    }
+}
---
+++ Proxy/Program.cs
@@ -0,0 +1,42 @@
+﻿/// The 'Subject' abstract class
+public abstract class Subject
+{
+    public abstract void Request();
+}
+
+/// The 'RealSubject' class
+public class RealSubject : Subject
+{
+    public override void Request()
+    {
+        Console.WriteLine("Called RealSubject.Request()");
+    }
+}
+
+/// The 'Proxy' class
+public class Proxy : Subject
+{
+    private RealSubject realSubject;
+    public override void Request()
+    {
+        // Use 'lazy initialization'
+        if (realSubject == null)
+        {
+            realSubject = new RealSubject();
+        }
+        realSubject.Request();
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Create proxy and request a service
+        Proxy proxy = new Proxy();
+        proxy.Request();
+        // Wait for user
+        Console.ReadKey();
+    }
+}
+
